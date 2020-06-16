9eac2574324faffd45aa570fe7aa0d0b19ab0218
Lucas Vieira Vicente <lucas@somosasoma.com.br>
2020-06-16 00:37:11 -0300

    Inserted folder model and created person class

---
+++ RestWithASPNET/Model/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNET.Model
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public Gender Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        [Display(Name = "Masculino")]
+        Masculine,
+        [Display(Name = "Feminino")]
+        Female
+    }
+}
