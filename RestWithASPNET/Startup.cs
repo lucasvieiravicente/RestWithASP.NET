a8e90599d62927869b6c201f18408887db20f2a3
Lucas Vieira Vicente <lucas@somosasoma.com.br>
2020-06-18 18:32:12 -0300

    Created connection with mySql database

--- RestWithASPNET/Startup.cs
+++ RestWithASPNET/Startup.cs
@@ -6,10 +6,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using RestWithASPNET.Model.Context;
 using RestWithASPNET.Services;
 using RestWithASPNET.Services.Interfaces;
 
@@ -27,6 +30,8 @@ namespace RestWithASPNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("MySqlConnectionString");
+            services.AddDbContext<MySqlContext>(options => options.UseMySql(connection));
             services.AddControllers();
 
             services.AddScoped<IPersonService, PersonService>();
